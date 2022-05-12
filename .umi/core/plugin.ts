8e2db5f56a1e5f253a112e8dedace7e5077b6fd4
老腰 <jobrzy@163.com>
2022-05-12 13:02:59 +0800

    feat: 新增记录

--- .umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/usr/local/lib/node_modules/dumi/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu',],
-});
-
-export { plugin };
